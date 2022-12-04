2f0f17d751981d77e1a6afd1a95bf65ef1438c7b
Pavel Cof <cubalab@gmail.com>
2022-12-04 19:24:38 +0300

    H and M

--- Lessons/HashcodeAndEquals/HashcodeAndEquals.java
+++ Lessons/HashcodeAndEquals/HashcodeAndEquals.java
@@ -7,16 +7,57 @@ import java.util.Set;
 
 public class HashcodeAndEquals {
     public static void main(String[] args) {
-        Map<Integer,String> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
+        Map<Person,String> map = new HashMap<>();
+        Set<Person> set = new HashSet<>();
 
-        map.put(1,"Один");
-        map.put(1,"Два");
+        Person person =new Person(1,"Lisa");
+        Person person2 =new Person(1,"Ulya");
 
-        set.add(1);
-        set.add(1);
+        map.put(person,"Один");
+        map.put(person,"Два");
+
+        set.add(person);
+        set.add(person2);
 
         System.out.println(map);
         System.out.println(set);
+    }
+
+
+}
+
+class Person{
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name=name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (id != person.id) return false;
+        return name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
